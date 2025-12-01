72f4637fea10bb110124e9ac229588d5845afcfb
nzuluu <nisayblu2@gmail.com>
2025-12-01 00:17:10 -0800

    Wrote the function for repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = '';
+    if (num > 0) {
+        while(num > 0) {
+            finalString += string;
+            num--;
+        }
+    }
+    else if (num == 0) {
+        return finalStrin = '';
+    }
+    else {
+        return finalString = 'ERROR';
+    }
+    return finalString;
 };
 
 // Do not edit below this line
