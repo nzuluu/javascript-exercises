cb26c0cd4ccc3d759acf8a9ad62157b6739ccc86
nzuluu <nisayblu2@gmail.com>
2025-12-01 00:28:57 -0800

    Wrote reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tempHold = '';
+    let i = 1;
+    
+    for(str in string) {
+        tempHold += string.charAt(string.length-(i++));
+    }
+    return tempHold;
 };
 
 // Do not edit below this line
