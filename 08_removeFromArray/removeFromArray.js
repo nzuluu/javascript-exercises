bff6b6426d6f02ca47a2575f846ce44f2296a639
nzuluu <nisayblu2@gmail.com>
2025-12-02 15:56:58 -0800

    wrote the method for removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeNum) {
+    return arr.filter(item => !removeNum.includes(item))
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
