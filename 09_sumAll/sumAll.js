4b3a1b2076b18181bc11c3df18d630208e96e158
nzuluu <nisayblu2@gmail.com>
2025-12-02 21:59:10 -0800

    All the test passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,9 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
-    if (num1 > num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return sum = 'ERROR';
+    }
+    else if (num1 > num2) {
         for(let i = num2; i <= num1; i++) {
             sum += i;
         }
