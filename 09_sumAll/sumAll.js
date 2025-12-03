68fb4478fc45060efaf4020ec44379a06a1c5195
nzuluu <nisayblu2@gmail.com>
2025-12-02 18:02:59 -0800

    passed the 2nd test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,20 +1,17 @@
 const sumAll = function(num1,num2) {
-    let i = 1;
-    let arr = [];
+    let sum = 0;
     if (num1 > num2) {
-        for(; i < num1; i++) {
-            arr[i] = num2++;
+        for(let i = num2; i <= num1; i++) {
+            sum += i;
         }
     }
     else {
-        for (;i < num2; i++) {
-            arr[i] = num1++;
+        for (let i = num1;i <= num2; i++) {
+            sum += i;
         }
     }
     
-    let sumOfAllNums = arr.reduce((sum, current) => sum + current, 0);
-
-    return sumOfAllNums;
+    return sum;
 };
 
 // Do not edit below this line
