32102ada9be1a3bc969959afe1d412fdbed96082
nzuluu <nisayblu2@gmail.com>
2025-12-02 16:36:51 -0800

    Wrote method for sumAll function 1 test passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let i = 1;
+    let arr = [];
+    if (num1 > num2) {
+        for(; i < num1; i++) {
+            arr[i] = num2++;
+        }
+    }
+    else {
+        for (;i < num2; i++) {
+            arr[i] = num1++;
+        }
+    }
+    
+    let sumOfAllNums = arr.reduce((sum, current) => sum + current, 0);
 
+    return sumOfAllNums;
 };
 
 // Do not edit below this line
