9f2a67561496f34f71030011f98b695a96b3bff2
nzuluu <nisayblu2@gmail.com>
2026-03-16 16:27:35 -0700

    finished the exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let toNum = parseInt(num);
+    if (toNum < 0) {return "OOPS"}
+    if (toNum < 2) {return toNum};
 
+    let n1 = 0, n2 = 1, nextTerm;
+
+    for ( let i = 1; i < toNum; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return nextTerm;
+
+    //Instead of for loop can use recursive calls
+    /*     
+    return fibonacci(toNum - 1) + fibonacci(toNum- 2);
+    */
 };
 
 // Do not edit below this line
