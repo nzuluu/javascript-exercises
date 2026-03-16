453b2e0fbc10d8351b35a84deb7e2f369dd8fa77
nzuluu <nisayblu2@gmail.com>
2026-03-16 15:21:59 -0700

    finished the exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    /* remove special characters, spaces and make lowercase*/
+    let removeChar = word.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
+    /* reverse removeChar for comparison*/
+    let  checkPalindrome = removeChar.split('').reverse().join('');
+
+    for(let i = 0; i < word.length; i++) {
+        if (removeChar[i] !== checkPalindrome[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
