959c38c94f7a3b22d04a42d1232773c569cadab7
nzuluu <nisayblu2@gmail.com>
2026-03-16 02:12:31 -0700

    finished this activity

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum,current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((sum,current) => sum * current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = 1;
+	if (a > 1) {
+    for(let i = a; i >= 1; i--) {
+     answer = answer * i;
+    }
+    return answer;
+  }
+  else if (a == 0 || a == 1) {
+    return answer;
+  }
+  else {
+    return "number has to be positive";
+  }
 };
 
 // Do not edit below this line
